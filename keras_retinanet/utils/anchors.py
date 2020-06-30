9683c89097eb4daf33c53cc2fc31f7a121cdd3b4
Jasper Brown <jasperebrown@gmail.com>
2020-06-30 14:02:32 +1000

    changed default anchors

--- keras_retinanet/utils/anchors.py
+++ keras_retinanet/utils/anchors.py
@@ -43,9 +43,9 @@ class AnchorParameters:
 The default anchor parameters.
 """
 AnchorParameters.default = AnchorParameters(
-    sizes   = [32, 64, 128, 256, 512],
+    sizes   = [8, 32, 128, 256, 512],
     strides = [8, 16, 32, 64, 128],
-    ratios  = np.array([0.5, 1, 2], keras.backend.floatx()),
+    ratios  = np.array([0.8, 1, 1.2], keras.backend.floatx()),
     scales  = np.array([2 ** 0, 2 ** (1.0 / 3.0), 2 ** (2.0 / 3.0)], keras.backend.floatx()),
 )
 
@@ -221,6 +221,8 @@ def anchors_for_shape(
 
     if anchor_params is None:
         anchor_params = AnchorParameters.default
+        print("Using default anchor parameters of:")
+        print(anchor_params)
 
     if shapes_callback is None:
         shapes_callback = guess_shapes
